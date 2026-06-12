f81eae82cb79c731768c5e6087b594a0b1c5a8f1
Vansh Sharma <vansh28sharma2005@gmail.com>
2026-06-12 11:29:16 +0530

    Add First and Last Position solution

---
+++ binary_search/fs.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int firstocc(vector<int>& nums,int key,int size){
+    int start=0;
+    int end=size-1;
+    int mid=start+(end-start)/2;
+    int ans=-1;
+        while(start<=end){
+        if(nums[mid]==key){
+
+            ans=mid;
+            end=mid-1;
+            
+        }
+         else if(key<nums[mid]){
+
+            end=mid-1;
+
+        }
+        else{
+            start=mid+1;
+        }
+        mid=start+(end-start)/2;
+    }
+    return ans;
+
+}
+int lastocc(vector<int>& nums,int key,int size){
+    int start=0;
+    int end=size-1;
+    int ans=-1;
+    int mid=start+(end-start)/2;
+        while(start<=end){
+        if(nums[mid]==key){
+            ans=mid;
+            start=mid+1;
+        }
+         else if(key<nums[mid]){
+
+            end=mid-1;
+
+        }
+        else{
+            start=mid+1;
+        }
+        mid=start+(end-start)/2;
+    }
+    return ans;
+
+}
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {firstocc(nums,target,nums.size()),
+        lastocc(nums,target,nums.size())};
+    }
+};
