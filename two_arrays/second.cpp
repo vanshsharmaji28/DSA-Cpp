16ad603ffa717d62bf4e16cfcdb3283001010482
Vansh Sharma <vansh28sharma2005@gmail.com>
2026-06-26 23:50:28 +0530

    2d arrays

---
+++ two_arrays/second.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<numeric>
+using namespace std;
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int totalsum=accumulate(nums.begin(),nums.end(),0);
+        int leftsum=0;
+
+        for(int i=0;i<nums.size();i++){
+            int rightsum=totalsum-leftsum-nums[i];
+
+            
+            if(leftsum==rightsum){
+                return i;
+            }
+            leftsum+=nums[i];
+        }
+        return -1;
+    }
+};
